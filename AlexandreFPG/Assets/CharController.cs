1fbe3416d731f33911c2b3745ba21cfae9ba333f
Alexandre220990 <drike.alexandre22@gmail.com>
2022-01-21 11:28:55 +0000

    Added script for Character control Basic key pressing movements Mouse movements Added some animations Some animations will change Signed-off-by: Alexandre220990 <drike.alexandre22@gmail.com>

---
+++ AlexandreFPG/Assets/CharController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    private float current_speed;
+    private float WALKING_SPEED = 1,RUNNING_SPEED = 3;
+    private float turning_speed = 90;
+    private float mouse_sensitivity_x = 180;
+    // Start is called before the first frame update
+    void Start()
+    {
+        current_speed = WALKING_SPEED;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (should_move_forward()) move_forward();
+        if (should_move_backward()) move_backward();
+        if (should_turn_left()) turn_left();
+
+        turn(Input.GetAxis("Horizontal"));
+    }
+
+    private void turn(float mouse_turn_value_x)
+    {
+        transform.Rotate(Vector3.up, mouse_sensitivity_x * mouse_turn_value_x * Time.deltaTime);
+    }
+
+    private void turn_left()
+    {
+        transform.Rotate(Vector3.up, -turning_speed * Time.deltaTime);
+       
+    }
+
+    private bool should_turn_left()
+    {
+        return Input.GetKey(KeyCode.A);
+
+    }
+    private void move_backward()
+    {
+        transform.position -= current_speed * transform.forward * Time.deltaTime;
+    }
+
+    private bool should_move_backward()
+    {
+        return Input.GetKey(KeyCode.S);
+    }
+    private void move_forward()
+    {
+        //move in F.R.I using s = u * p
+        transform.position += current_speed * transform.forward * Time.deltaTime;
+    }
+
+    private bool should_move_forward()
+    {
+        return Input.GetKey(KeyCode.W);
+    }
+}
