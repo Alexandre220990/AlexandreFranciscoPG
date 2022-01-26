d80d426b0ea359dfbd3269179109ebc19d110b59
Alexandre220990 <drike.alexandre22@gmail.com>
2022-01-26 17:47:11 +0000

    Created character script created player camera scrip created manager script configuration of camera in relation with character Signed-off-by: Alexandre220990 <drike.alexandre22@gmail.com>

--- AlexandreFPG/Assets/CharController.cs
+++ AlexandreFPG/Assets/CharController.cs
@@ -9,11 +9,17 @@ public class CharController : MonoBehaviour
     private float WALKING_SPEED = 1,RUNNING_SPEED = 3;
     private float turning_speed = 90;
     private float mouse_sensitivity_x = 180;
+
+    PlayerCamera my_camera;
+
     // Start is called before the first frame update
     void Start()
     {
         current_speed = WALKING_SPEED;
         
+        my_camera = GetComponentInChildren<PlayerCamera>();
+        my_camera.you_belong_to(this);
+
     }
 
     // Update is called once per frame
@@ -21,10 +27,17 @@ public class CharController : MonoBehaviour
     {
         if (should_move_forward()) move_forward();
         if (should_move_backward()) move_backward();
+
         if (should_turn_left()) turn_left();
         if (should_turn_right()) turn_right();
 
         turn(Input.GetAxis("Horizontal"));
+        adjust_camera(Input.GetAxis("Vertical"));
+    }
+
+    private void adjust_camera(float vertical_adjustment)
+    {
+        my_camera.adjust_vertical_angle(vertical_adjustment);
     }
 
     private void turn(float mouse_turn_value_x)
@@ -39,7 +52,7 @@ public class CharController : MonoBehaviour
     }
     private bool should_turn_right()
     {
-        return Input.GetKey(KeyCode.D);
+        return Input.GetKey(KeyCode.A);
     }
 
     private void turn_left()
@@ -50,7 +63,7 @@ public class CharController : MonoBehaviour
 
     private bool should_turn_left()
     {
-        return Input.GetKey(KeyCode.A);
+        return Input.GetKey(KeyCode.D);
 
     }
     private void move_backward()
