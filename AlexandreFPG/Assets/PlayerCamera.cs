559deb994fec354bd489ad9714a04da14b7f358b
STUDENT Alexandre Francisco <Alexandre.Francisco@students.ittralee.ie>
2022-02-08 15:45:47 +0000

    Some changes in the character controller(walk,jump,run) some changes on the character camera(3rd person) changes in the mouse sensitivity some more changes will be done Signed-off-by: STUDENT Alexandre Francisco <Alexandre.Francisco@students.ittralee.ie>

--- AlexandreFPG/Assets/PlayerCamera.cs
+++ AlexandreFPG/Assets/PlayerCamera.cs
@@ -5,23 +5,24 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    float angle = 0f, distance = 5f;
+    float angle = 0f, distance = 15f, camera_height = 5f;
     private Vector3 desired_camera_position;
     Transform owning_character_transform;
     private CharController owning_character;
-    private float SENSITIVITY_VERTICAL_ROTATE = 0.01f;
+    private float SENSITIVITY_VERTICAL_ROTATE = 0.003f;
     private float focal_height = 0.2f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //owning_character = FindObjectOfType<CharController>();
+        //desired_camera_position = new Vector3(0, 1, 10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.localPosition = Vector3.Lerp(transform.localPosition, desired_camera_position, 0.01f);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, desired_camera_position, 0.1f);
         transform.LookAt(owning_character_transform.position + focal_height * Vector3.up);
         print(Mathf.Rad2Deg * angle);
     }
@@ -37,7 +38,7 @@ public class PlayerCamera : MonoBehaviour
         angle -= SENSITIVITY_VERTICAL_ROTATE * vertical_adjustment;
         angle = Mathf.Clamp(angle, -2, 0);
 
-        desired_camera_position = new Vector3(0, distance * Mathf.Cos(angle), distance * Mathf.Sin(angle));
+        desired_camera_position = camera_height * Vector3.up + new Vector3(0, distance * Mathf.Cos(angle), distance * Mathf.Sin(angle));
     }
 
 }
