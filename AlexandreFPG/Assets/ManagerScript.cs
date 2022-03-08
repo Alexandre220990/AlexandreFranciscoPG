db6a8ac2a7f13dc484cfba8e9a91febcea06cb10
STUDENT Alexandre Francisco <Alexandre.Francisco@students.ittralee.ie>
2022-03-08 15:52:52 +0000

    EnemyController (changes in the enemy vs target) Start building Main Menu Signed-off-by: STUDENT Alexandre Francisco <Alexandre.Francisco@students.ittralee.ie>

--- AlexandreFPG/Assets/ManagerScript.cs
+++ AlexandreFPG/Assets/ManagerScript.cs
@@ -15,7 +15,7 @@ public class ManagerScript : MonoBehaviour
     {
         zombies = new List<enemyController>();
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 5; i++)
         {
             //GameObject g = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
